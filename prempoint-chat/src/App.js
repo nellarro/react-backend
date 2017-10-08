9d6f81c44f56aa7f1515b704eed5a5491e95beb9
Jenell Pizarro <nellarro@gmail.com>
2017-10-07 20:19:25 -0400

    it works. ugly but it works

--- prempoint-chat/src/App.js
+++ prempoint-chat/src/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 import io from 'socket.io-client'
+
+// let message = {
+//   type : 'message',
+//   text : this.state.text,
+//   // time : 0, // Set by the server
+//   // user : 0, // Set before sending
+// } 
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       text: '',
+      messages: [],
       users: []
     }
   }
@@ -18,42 +26,51 @@ class App extends Component {
   componentDidMount(){
     fetch('http://localhost:3001/users')
       .then(res => res.json())
-      .then(users => this.setState({ users }))
+      .then(users => this.setState({ users })
+    )
+    this.socket = io('http://localhost:3001') 
+    // makes sure that user receives messages upon "logging" in
+    this.socket.on('broadcasting chat message', msg => {
+      this.setState({messages: [...this.state.messages, msg]})
+      console.log(this.state.messages)
+    })   
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    if (this.state.text !== '') {
+      this.socket.emit('chat is working', this.state.text)
+
+      this.setState({ text: '' })
+    }
   }
 
 
   render() {
-    let socket = io('http://localhost:3001');
-
-    let handleSubmit = e => {
-      e.preventDefault()
-      if (this.state.text !== '') {
-        let message = {
-          type : 'message',
-          text : this.state.text,
-          time : 0, // Set by the server
-          user : 0, // Set before sending
-        } 
-        this.setState({ text: '' });
-      }
-    }
-
+    
     let handleChange = e => {
-      this.setState({ text : e.target.value });
+      this.setState({ text : e.target.value })
+      // console.log(e)
     }
     
     return (
       <div className="App">
-          <h1 className="App-title">users</h1>
+          <h1 className="App-title">RIP Aim</h1>
           {this.state.users
             .map(user => <div key={user.id}>{user.username}</div>
           )}
-          <ul id="messages"></ul>
-          <form onSubmit={handleSubmit} action="">
-            <input id="m" autoComplete="off" onChange={handleChange} /> <button onSubmit={handleSubmit}>Send</button>
+          <ul id="messages">
+            {this.state.messages
+              .map((message, i) => {
+                return <li key={i}>{message}</li>
+              })
+            }
+          </ul>
+          <form onSubmit={this.handleSubmit} action="">
+            <input id="m" autoComplete="off" onChange={handleChange} /> <button onSubmit={this.handleSubmit}>Send</button>
           </form>
       </div>
-    );
+    )
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,22 @@ let app = require('express')()
 let http = require('http').Server(app)
 let io = require('socket.io')(http)
 
-let port = 3001
-
 /* GET home page. */
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
+// handles the messages
 io.on('connection', socket => {
-  console.log('a user is connected')
+  socket.on('chat is working', msg => {
+    console.log('message: ' + msg)
+    io.emit('broadcasting chat message', msg)
+  } )
+    console.log('a user is connected')
 })
 
 http.listen(3001, () => {
   console.log('port 3001')
 })
-
-server.listen(port, err => {  
-  err 
-    ? console.log('error:', err) 
-    : open(`http://localhost:${port}`)
-   }
-);
 
 module.exports = app;
